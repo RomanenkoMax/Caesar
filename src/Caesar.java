e5a75b5cab9f3a186d3df3fff8cad39e0807febb
Romanenko Max <kiev33@i.ua>
2016-02-29 00:11:09 +0200

    Next commit

--- src/Caesar.java
+++ src/Caesar.java
@@ -2,35 +2,45 @@ public class Caesar {
     String mesage;
     private char abc [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     void encode (String mesage, int n){
-        this.mesage = mesage;
+        this.mesage = mesage.toLowerCase();
         StringBuilder encodeMesage = new StringBuilder(mesage);
         for (int i = 0; i < mesage.length(); i++) {
-            char index = mesage.charAt(i);
+            char index = this.mesage.charAt(i);
+            if (index == ' '){
+                encodeMesage.setCharAt(i, ' ');
+                continue;
+            } else {
             int number = index - 'a';
             encodeMesage.setCharAt(i, abc[((number + n) % 26)]);
+            }
         }
         System.out.println(encodeMesage);
     }
     void decode (String mesage, int n){
-        this.mesage = mesage;
+        this.mesage = mesage.toLowerCase();
         StringBuilder decodeMesage = new StringBuilder(mesage);
         for (int i = 0; i < mesage.length(); i++){
-            char index = mesage.charAt(i);
-            int number = index - 'a';
-            if ((number - n) < 0) decodeMesage.setCharAt(i, abc[(26 - n + number)]);
-            else decodeMesage.setCharAt(i, abc[(number - n) % 26]);
-            //System.out.println(abc[(number - n) % 26]);
-            //System.out.println(number);
-            //System.out.println(mesage);
-            //System.out.println(n);
+            char index = this.mesage.charAt(i);
+            if (index == ' '){
+                decodeMesage.setCharAt(i, ' ');
+                continue;
+            } else {
+                int number = index - 'a';
+                if ((number - n) < 0) decodeMesage.setCharAt(i, abc[(26 - n + number)]);
+                else decodeMesage.setCharAt(i, abc[(number - n) % 26]);
+                //System.out.println(abc[(number - n) % 26]);
+                //System.out.println(number);
+                //System.out.println(mesage);
+                //System.out.println(n);
+            }
         }
         System.out.println(decodeMesage);
 
     }
     public static void main (String [] args){
         Caesar newMesage = new Caesar();
-        newMesage.encode("text", 5);
-        newMesage.decode("yjcy", 5);
+        newMesage.encode("text for Encoding", 5);
+        newMesage.decode("yjcy ktw Jshtinsl", 5);
 
     }
 
