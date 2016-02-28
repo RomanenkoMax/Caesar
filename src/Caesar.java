2f0b33d3e2c211bc522ae8dbd3cd69b6df4e92e4
Romanenko Max <kiev33@i.ua>
2016-02-28 23:25:26 +0200

    Second commit

--- src/Caesar.java
+++ src/Caesar.java
@@ -1,12 +1,12 @@
 public class Caesar {
     String mesage;
-    private char abc [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','v','z'};
+    private char abc [] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
     void encode (String mesage, int n){
         this.mesage = mesage;
         StringBuilder encodeMesage = new StringBuilder(mesage);
         for (int i = 0; i < mesage.length(); i++) {
             char index = mesage.charAt(i);
-            int number = Integer.valueOf(index - 'a');
+            int number = index - 'a';
             encodeMesage.setCharAt(i, abc[((number + n) % 26)]);
         }
         System.out.println(encodeMesage);
@@ -16,14 +16,22 @@ public class Caesar {
         StringBuilder decodeMesage = new StringBuilder(mesage);
         for (int i = 0; i < mesage.length(); i++){
             char index = mesage.charAt(i);
-            int number = Integer.valueOf(index - 'a');
-            decodeMesage.setCharAt(i, abc[((number - n) % 26)]);
+            int number = index - 'a';
+            if ((number - n) < 0) decodeMesage.setCharAt(i, abc[(26 - n + number)]);
+            else decodeMesage.setCharAt(i, abc[(number - n) % 26]);
+            //System.out.println(abc[(number - n) % 26]);
+            //System.out.println(number);
+            //System.out.println(mesage);
+            //System.out.println(n);
         }
         System.out.println(decodeMesage);
+
     }
     public static void main (String [] args){
         Caesar newMesage = new Caesar();
-        newMesage.encode("aaa",2);
-        newMesage.decode("ccc",2);
+        newMesage.encode("text", 5);
+        newMesage.decode("yjcy", 5);
+
     }
+
 }
